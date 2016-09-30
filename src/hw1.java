c150b477978346faaaf47ea7e9d479ad7590a2ca
user <user@FF02>
2016-09-30 09:08:24 +0800

    Homework 1

--- src/hw1.java
+++ src/hw1.java
@@ -3,7 +3,7 @@ public class hw1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello OOP");
+		String lyrics = "Let \"her\" go! Let \"her\" go! Can't hold \"her\" back anymore";	
+		System.out.println(lyrics);
 	}
-
 }
